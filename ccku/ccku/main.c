f59f62f2809b422e917ac88a2349bd291ad2a76c
Chris Morrison <chris-morrison@cyberservices.com>
2014-05-24 13:15:55 +0100

    Added Xcode project

---
+++ ccku/ccku/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ccku
+//
+//  Created by Chris Morrison on 24/05/2014.
+//  Copyright (c) 2014 Chris Morrison. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
